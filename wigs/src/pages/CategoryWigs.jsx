793c73db727d9ff216817d83a90639af64a6b637
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-10-03 09:57:15 -1200

    Update and rename CategoryHair.jsx to CategoryWigs.jsx

--- wigs/src/pages/CategoryWigs.jsx
+++ wigs/src/pages/CategoryWigs.jsx
@@ -1,30 +1,28 @@
 import React, { useContext } from 'react';
-import { MyContext } from '../context/contextProvider';
+import { StoreContext } from '../context/contextProvider';
 import { useNavigate } from 'react-router-dom';
 
 const CategoryWigs = () => {
   const navigate = useNavigate();
-  const { WigsByCategory, setWigsByCategory } = useContext(MyContext);
+  const { wigsByCategory } = useContext(StoreContext);
 
   return (
     <div className="flex justify-center pt-5">
       <div className="w-[90%] flex flex-wrap gap-x-4 justify-center pt-2 gap-y-4 ">
-        {WigsByCategory["wigs"].map((wig, i) => (
+        {wigsByCategory.map((wig, i) => (
           <div
             key={i}
             className="sm:w-[24%] w-100 flex flex-col items-center gap-y-3 cursor-pointer"
           >
-            {/* Image of Wig */}
             <img
-              onClick={() => navigate(`./wigs/${wig.wigName}`)}
-              src={wig.wigThumb}
-              alt={wig.wigName}
+              onClick={() => navigate(`/wigtypes/${wig.name}`)}
+              src={wig.image}
+              alt={wig.name}
               className="w-full h-full object-cover rounded-md hover:scale-105 ease-in-out duration-300"
             />
-            {/* Wig Name */}
             <div className="h-[10%]">
               <h1 className="font-bold w-[80%] text-center">
-                {wig.wigName}
+                {wig.name}
               </h1>
             </div>
           </div>

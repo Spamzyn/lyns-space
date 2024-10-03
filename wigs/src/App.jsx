b91e9b62fba3fd07cdf9f147e20d2f04aadf0de4
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-10-03 09:41:57 -1200

    Update App.jsx

--- wigs/src/App.jsx
+++ wigs/src/App.jsx
@@ -3,28 +3,25 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-
-import WigTypes from "./pages/WigTypes"; // Updated from Recipes to WigTypes
+import WigTypes from "./pages/WigTypes";
 import Navbar from "./layouts/Navbar";
 import Footer from "./layouts/Footer";
 import { MyProvide } from "./context/contextProvider";
-import WigDetail from "./pages/WigDetail"; // Updated from MealRecipe to WigDetail
-import CategoryWigs from "./pages/CategoryWigs"; // Updated from CategoryMeals to CategoryWigs
+import WigDetail from "./pages/WigDetail";
+import CategoryWigs from "./pages/CategoryWigs";
 
 const App = () => {
   return (
-    <div className="sm-px-[5vw] md-px-[7vw] lg-px-[9vw]">
+    <div className="sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
       <Navbar />
       <MyProvide>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-
-          <Route path="/wigtypes" element={<WigTypes />} /> {/* Updated from /recipes to /wigtypes */}
-          <Route path="/wigtypes/:wigName" element={<WigDetail />} /> {/* Updated from /recipes/:mealName to /wigtypes/:wigName */}
-          <Route path="/:category" element={<CategoryWigs />} /> {/* Updated from CategoryMeals to CategoryWigs */}
-
+          <Route path="/wigtypes" element={<WigTypes />} />
+          <Route path="/wigtypes/:wigName" element={<WigDetail />} />
+          <Route path="/:category" element={<CategoryWigs />} />
         </Routes>
       </MyProvide>
       <Footer />

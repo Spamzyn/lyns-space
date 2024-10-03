afbdfc53524b95d3e6334ae745b00d8ff268edcd
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-10-03 09:51:04 -1200

    Update App.jsx

--- wigs/src/App.jsx
+++ wigs/src/App.jsx
@@ -4,9 +4,11 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import WigTypes from "./pages/WigTypes";
+import WigStyles from "./pages/WigStyles";
+import CareGuide from "./pages/CareGuide";
 import Navbar from "./layouts/Navbar";
 import Footer from "./layouts/Footer";
-import { MyProvide } from "./context/contextProvider";
+import { StoreProvider } from "./context/contextProvider";
 import WigDetail from "./pages/WigDetail";
 import CategoryWigs from "./pages/CategoryWigs";
 
@@ -14,16 +16,18 @@ const App = () => {
   return (
     <div className="sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
       <Navbar />
-      <MyProvide>
+      <StoreProvider>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/wigtypes" element={<WigTypes />} />
+          <Route path="/styles" element={<WigStyles />} />
+          <Route path="/care-guide" element={<CareGuide />} />
           <Route path="/wigtypes/:wigName" element={<WigDetail />} />
           <Route path="/:category" element={<CategoryWigs />} />
         </Routes>
-      </MyProvide>
+      </StoreProvider>
       <Footer />
     </div>
   );

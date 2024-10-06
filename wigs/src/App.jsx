ca99e96fd8d803a8bd3e96ebaf13968aa05accab
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-10-05 21:29:21 -1200

    Update App.jsx

--- wigs/src/App.jsx
+++ wigs/src/App.jsx
@@ -3,14 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import WigTypes from "./pages/WigTypes";
+import WigTypes from './pages/WigTypes';
 import WigStyles from "./pages/WigStyles";
 import CareGuide from "./pages/CareGuide";
 import Navbar from "./layouts/Navbar";
 import Footer from "./layouts/Footer";
 import { StoreProvider } from "./context/contextProvider";
 import WigDetail from "./pages/WigDetail";
-import CategoryWigs from "./pages/CategoryWigs";
 
 const App = () => {
   return (
@@ -25,7 +24,6 @@ const App = () => {
           <Route path="/styles" element={<WigStyles />} />
           <Route path="/care-guide" element={<CareGuide />} />
           <Route path="/wigtypes/:wigName" element={<WigDetail />} />
-          <Route path="/:category" element={<CategoryWigs />} />
         </Routes>
       </StoreProvider>
       <Footer />
